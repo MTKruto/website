13e3fcf661b65f65ab86ec527be5133d784bc33d
Roj <rojvv@icloud.com>
2026-05-10 23:22:11 +0300

    Add Guest Queries Methods section

--- _gen.tsx
+++ _gen.tsx
@@ -114,6 +114,7 @@ for (const dir of [`src${version}/methods`, `src${version}/types`]) {
   const groups = Object.entries({
     "ac": ["Account", "account"],
     "ms": ["Messages", "messages"],
+    "gq": ["Guest Queries", "guest-queries"],
     "mb": ["Managed Bots", "managed-bots"],
     "ss": ["Sticker Sets", "sticker-sets"],
     "ns": ["Network Statistics", "network-statistics"],
